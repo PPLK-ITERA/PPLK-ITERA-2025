a0e3b03d713446b8957e299dc8463351269cfc63
almajulioo <134611731+almajulioo@users.noreply.github.com>
2024-07-02 19:42:00 +0700

    feat: menambahkan fungsi cru pada pplk controller

--- app/Http/Controllers/PresensiPplkController.php
+++ app/Http/Controllers/PresensiPplkController.php
@@ -2,9 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\PresensiPplk;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class PresensiPplkController extends Controller
 {
     //
+    public function getAllPresensi(){
+        $presensi = PresensiPplk::all();
+        return response()->json($presensi, 200);
+    }
+
+    public function getUserPresensi($user_id){
+        $presensi = PresensiPplk::where('user_id', $user_id)->get();
+        return response()->json($presensi, 200);
+    }
+
+    public function store(Request $request){
+        $request->validate([
+            'user_id'=>'required',
+            'tanggal_presensi'=>'required',
+            'kehadiran'=>'required',
+        ]);
+
+        $presensi = PresensiPplk::create(
+                [
+                    'user_id' => $request->user_id,
+                    'tanggal_presensi' => Carbon::today(),
+                    'kehadiran' => $request->kehadiran,
+                    'keterangan' => $request->keterangan
+                ]
+            );
+
+        return response()->json($presensi, 200);
+    }
+
+    public function updateKehadiran(Request $request, $user_id, $tanggal_presensi){
+        $presensi = PresensiPplk::where('user_id', $user_id)->where('tanggal_presensi', $tanggal_presensi);
+
+        $presensi->update([
+            'kehadiran' => $request->kehadiran
+        ]);
+
+        return response()->json($presensi, 200);
+    }
 }
