fa821b0e8f3a31a2ff19b31f30edade7c7807bd1
almajulioo <134611731+almajulioo@users.noreply.github.com>
2024-07-02 19:18:37 +0700

    feat: membuat crud pada booklet controller

--- app/Http/Controllers/BookletController.php
+++ app/Http/Controllers/BookletController.php
@@ -2,9 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Booklet;
 use Illuminate\Http\Request;
 
 class BookletController extends Controller
 {
-    //
+    public function index()
+    {
+        $booklet = Booklet::all();
+        return response()->json($booklet, 200);
+    }
+
+    public function store(Request $request){
+        $booklet = Booklet::create(
+            [
+                'nama_booklet,' => $request->nama_booklet,
+                'url_booklet,' => $request->url_booklet,
+            ]
+        );
+
+        return response()->json($booklet,200);
+    }
+
+    public function update(Request $request, $id){
+        $booklet = Booklet::find($id);
+        $booklet->update(
+            [
+                'nama_booklet,' => $request->nama_booklet,
+                'url_booklet,' => $request->url_booklet,
+            ]
+        );
+        return response()->json($booklet,200);
+    }
+
+    public function delete($id){ 
+        $booklet = Booklet::find($id);
+        $booklet->delete();
+        return response()->json($booklet,200);
+    }
 }
