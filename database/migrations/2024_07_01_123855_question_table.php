dbf12d7189f7e810fa66da64758d1994f70cb81a
FadilRifqi <fadilataullahrifqi@gmail.com>
2024-07-02 23:52:57 +0700

    feat: menambahkan kolom timestamp pada table question

--- database/migrations/2024_07_01_123855_question_table.php
+++ database/migrations/2024_07_01_123855_question_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
             $table->text('teks_pertanyaan');
+            $table->timestamps();
         });
     }
 
