2aa1b0b6de294f69d39790e60200ffa9fab194a2
Stevanus Cahya Anggara <stevanuschyaanggara@gmail.com>
2024-07-03 12:03:04 +0700

    Update : Menambahkan Role Model,Table,Middleware

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
     ];
 
     /**
@@ -42,6 +43,10 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'isFirstTime' => 'boolean',
         ];
+    }
+    public function role(){
+        return $this->belongsTo(Role::class);
     }
 }
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->foreignId('role');
+            $table->boolean('isFirstTime')->default(true);
             $table->rememberToken();
             $table->timestamps();
         });
