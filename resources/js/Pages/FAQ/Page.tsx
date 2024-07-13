407a4f598e4f773e037d710d4a453bb9f6645015
Wupxy <muhammad.122140193@student.itera.ac.id>
2024-07-13 19:12:40 +0700

    refactor:remove button(mau tanya sesuatu)

--- resources/js/Pages/FAQ/Page.tsx
+++ resources/js/Pages/FAQ/Page.tsx
@@ -1,5 +1,4 @@
 import { faqs } from "@/lib/data/faq";
-import { useAos } from "@/lib/hooks/useAos";
 
 import React, { useEffect, useState } from "react";
 
@@ -36,11 +35,7 @@ const Page: React.FC = () => {
     );
 
     return (
-        <div
-            data-aos="fade-left"
-            data-aos-duration="3000"
-            className="bg-pattern-white relative flex flex-col w-full min-h-screen text-center"
-        >
+        <div className="bg-pattern-white relative flex flex-col w-full min-h-screen text-center">
             <div>
                 <Navbar isSolid={true} />
                 <h2 className="mt-[140px] font-avigea text-[39px] text-jaffa-900">
@@ -81,22 +76,6 @@ const Page: React.FC = () => {
                         onPageChange={setCurrentPage}
                     />
                 </div>
-            </div>
-            <div className="relative">
-                <button className="absolute bottom-24 right-8 flex items-center px-5 py-4 bg-gradient-to-r from-[#F24822] to-[#8C2A14] text-white rounded-tl-[22px] rounded-tr-[22px] rounded-bl-[22px] rounded-br-[0px]  focus:outline-none ">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="currentColor"
-                        className="icon icon-tabler icons-tabler-filled icon-tabler-message mr-2"
-                    >
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                        <path d="M18 3a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-4.724l-4.762 2.857a1 1 0 0 1 -1.508 -.743l-.006 -.114v-2h-1a4 4 0 0 1 -3.995 -3.8l-.005 -.2v-8a4 4 0 0 1 4 -4zm-4 9h-6a1 1 0 0 0 0 2h6a1 1 0 0 0 0 -2m2 -4h-8a1 1 0 1 0 0 2h8a1 1 0 0 0 0 -2" />
-                    </svg>
-                    Mau Tanya Sesuatu?
-                </button>
             </div>
             <div className="text-left">
                 <Footer />
