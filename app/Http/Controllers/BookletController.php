ce3ed97a5f96ff70b1fa72bf90e579ac2556c1d1
almajulioo <134611731+almajulioo@users.noreply.github.com>
2024-07-22 22:53:55 +0700

    refactor: menambahkan db transaction pada booklet

--- app/Http/Controllers/BookletController.php
+++ app/Http/Controllers/BookletController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Booklet;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\DB;
 
 class BookletController extends Controller
 {
@@ -26,30 +27,35 @@ class BookletController extends Controller
          'nama_booklet' => 'required|string',
          'url_booklet' => 'required|string',
       ]);
+      DB::beginTransaction();
+      try{
+         $booklet = Booklet::create(
+            [
+               'nama_booklet' => $request->nama_booklet,
+               'url_booklet' => $request->url_booklet,
+            ]
+         );
 
-      $booklet = Booklet::create(
-         [
-            'nama_booklet' => $request->nama_booklet,
-            'url_booklet' => $request->url_booklet,
-         ]
-      );
+         DB::commit();
 
-      if ($booklet) {
+         if ($booklet) {
+            return Inertia::render('Booklet/CarouselBooklet', [
+               'response' => [
+                  'status' => 201,
+                  'message' => 'Berhasil menambahkan booklet',
+                  'data' => $booklet
+               ]
+            ]);
+         }
+      } catch (\Exception $e) {
+         DB::rollBack();
          return Inertia::render('Booklet/CarouselBooklet', [
             'response' => [
-               'status' => 201,
-               'message' => 'Berhasil menambahkan booklet',
-               'data' => $booklet
+               'status' => 500,
+               'message' => 'Gagal menambahkan booklet',
             ]
          ]);
       }
-
-      return Inertia::render('Booklet/CarouselBooklet', [
-         'response' => [
-            'status' => 500,
-            'message' => 'Gagal menambahkan booklet',
-         ]
-      ]);
    }
 
    public function update(Request $request, $id)
@@ -59,51 +65,63 @@ class BookletController extends Controller
          'url_booklet' => 'required|string',
       ]);
 
-      $booklet = Booklet::find($id);
+      DB::beginTransaction();
+      try{
+         $booklet = Booklet::find($id);
+   
+         $booklet->update(
+            [
+               'nama_booklet' => $request->nama_booklet,
+               'url_booklet' => $request->url_booklet,
+            ]
+         );
 
-      $booklet->update(
-         [
-            'nama_booklet' => $request->nama_booklet,
-            'url_booklet' => $request->url_booklet,
-         ]
-      );
-
-      if ($booklet) {
+         DB::commit();
+   
+         if ($booklet) {
+            return Inertia::render('Booklet/CarouselBooklet', [
+               'response' => [
+                  'status' => 201,
+                  'message' => 'Berhasil menambahkan booklet',
+                  'data' => $booklet
+               ]
+            ]);
+         }
+      } catch(\Exception $e) {
+         DB::rollBack();
          return Inertia::render('Booklet/CarouselBooklet', [
             'response' => [
-               'status' => 201,
-               'message' => 'Berhasil menambahkan booklet',
-               'data' => $booklet
+               'status' => 500,
+               'message' => 'Gagal mengubah booklet',
             ]
          ]);
       }
 
-      return Inertia::render('Booklet/CarouselBooklet', [
-         'response' => [
-            'status' => 500,
-            'message' => 'Gagal mengubah booklet',
-         ]
-      ]);
    }
 
    public function delete($id)
    {
-      $booklet = Booklet::find($id)->delete();
-
-      if ($booklet) {
+      DB::beginTransaction();
+      try{
+         $booklet = Booklet::find($id)->delete();
+         DB::commit();
+         if ($booklet) {
+            return Inertia::render('Booklet/CarouselBooklet', [
+               'response' => [
+                  'status' => 201,
+                  'message' => 'Berhasil menghapus booklet',
+               ]
+            ]);
+         }
+      } catch(\Exception $e) {
+         DB::rollBack();
          return Inertia::render('Booklet/CarouselBooklet', [
             'response' => [
-               'status' => 201,
-               'message' => 'Berhasil menghapus booklet',
+               'status' => 500,
+               'message' => 'Gagal menghapus booklet',
             ]
          ]);
       }
 
-      return Inertia::render('Booklet/CarouselBooklet', [
-         'response' => [
-            'status' => 500,
-            'message' => 'Gagal menghapus booklet',
-         ]
-      ]);
    }
 }
