da12cd10a1183f05a719ea5c85f71d7d988492ea
Cornelius Linux <radi.adi78@gmail.com>
2024-07-24 06:09:47 +0700

    fix: atribut hilang

--- resources/js/Pages/Dashboard/cui/absensi/QRScanner.tsx
+++ resources/js/Pages/Dashboard/cui/absensi/QRScanner.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import Webcam from 'react-webcam';
-import { BrowserMultiFormatReader, NotFoundException } from '@zxing/library';
+import { NotFoundException } from '@zxing/library';
 import beep from '!assets/music/beep.mp3';
 import { Button } from '@/Components/ui/button';
 
@@ -15,10 +15,19 @@ interface QRScannerProps {
 const QRScanner: React.FC<QRScannerProps> = ({ onScan, onError, scanDelay, onBack, pause }) => {
     const webcamRef = useRef<Webcam>(null);
     const audioRef = useRef<HTMLAudioElement>(null);
-    const [codeReader, setCodeReader] = useState<BrowserMultiFormatReader | null>(null);
+    const [codeReader, setCodeReader] = useState<any>(null);
     const [scanning, setScanning] = useState(true);
     const [devices, setDevices] = useState<MediaDeviceInfo[]>([]);
     const [selectedDeviceId, setSelectedDeviceId] = useState<string | undefined>(undefined);
+
+    useEffect(() => {
+        const loadCodeReader = async () => {
+            const { BrowserMultiFormatReader } = await import('@zxing/library');
+            setCodeReader(new BrowserMultiFormatReader());
+        };
+
+        loadCodeReader();
+    }, []);
 
     useEffect(() => {
         const getDevices = async () => {
@@ -106,7 +115,7 @@ const QRScanner: React.FC<QRScannerProps> = ({ onScan, onError, scanDelay, onBac
                 </div>
             </div>
             <audio ref={audioRef} src={beep} />
-            
+
         </div>
     );
 };
