1707094f983c2ecefd412b514100361c747ad591
Cornelius Linux <radi.adi78@gmail.com>
2024-07-23 21:52:45 +0700

    refactor: reorganize routes

--- routes/ui.php
+++ routes/ui.php
@@ -5,28 +5,89 @@ use App\Http\Controllers\BookletController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+// =====================================
+// FAKULTAS (Guest)
+// =====================================
+Route::get('informasi/fakultas', function () {
+   return Inertia::render('Informasi/Fakultas/Page');
+})->name('informasi/fakultas/Sains');
+
+Route::get('informasi/fakultas/Ftik', function () {
+   return Inertia::render('Informasi/Fakultas/Ftik');
+})->name('informasi/fakultas/Ftik');
+
+Route::get('informasi/fakultas/Fti', function () {
+   return Inertia::render('Informasi/Fakultas/Fti');
+})->name('informasi/fakultas/Fti');
+
+
+// =====================================
+// PRODI (Guest)
+// =====================================
+Route::get('informasi/prodi', function () {
+   return Inertia::render('Informasi/Prodi/Page');
+})->name('informasi/prodi');
+
+Route::get('informasi/prodi/detail', function () {
+   return Inertia::render('Informasi/Prodi/Detail/Page');
+})->name('informasi/prodi/detail');
+
+
+// =====================================
+// UPT & UKM (Guest)
+// =====================================
+Route::get('informasi/upt', function () {
+   return Inertia::render('Informasi/Upt/Page');
+})->name('informasi/upt');
+
+Route::get('informasi/upt/{nama_upt}', function (string $nama_upt) {
+   return Inertia::render('Informasi/Upt/Detail/Page', [
+      'nama_upt' => $nama_upt
+   ]);
+});
+
+
+// =====================================
+// FAQ (Guest)
+// =====================================
 Route::get('faq', function () {
    return Inertia::render('FAQ/Page');
 })->name('faq');
 
+
+// =====================================
+// Ketentuan Atribut (Guest)
+// =====================================
 Route::get('ketentuan-atribut', function () {
    return Inertia::render('Atribut/Page');
 })->name('ketentuan-atribut');
 
+
+// =====================================
+// Dev Team (Guest)
+// =====================================
 Route::get('dev-team', function () {
    return Inertia::render('Dev/Page');
 })->name('dev-team');
 
+
+// =====================================
+// KM ITERA (Guest)
+// =====================================
 Route::get('informasi/km', function () {
    return Inertia::render('Informasi/Km/Page');
 })->name('informasi/km');
 
+
+// =====================================
+// UKM (Guest)
+// =====================================
 Route::get('informasi/ukm', function () {
    return Inertia::render('Informasi/Ukm/Page');
 })->name('informasi/ukm');
 
 Route::get('informasi/ukm/detail', function () {
-    return Inertia::render('Informasi/Ukm/Detail/Page');
+   return Inertia::render('Informasi/Ukm/Detail/Page');
 })->name('informasi/ukm/detail');
 
 Route::get('informasi/ukm/{nama_ukm}', function (string $nama_ukm) {
@@ -38,73 +99,135 @@ Route::get('informasi/pplk', function () {
    return Inertia::render('Informasi/Pplk/Page');
 })->name('informasi/pplk');
 
-Route::get('booklet', [BookletController::class, 'index'])->name('booklet');
 
+// =====================================
+// Booklet (Guest)
+// =====================================
+Route::get('booklet', [BookletController::class, 'guestIndex'])->name('booklet');
+
+
+// =====================================
+// Maskot (Guest)
+// =====================================
 Route::get('informasi/maskot', function () {
    return Inertia::render('Maskot/Page');
 })->name('informasi/maskot');
 
-Route::get('informasi/profil', function () {
-   return Inertia::render('Informasi/Profil/Page');
-})->name('informasi/profil');
-
 Route::middleware('auth')->group(function () {
+   // =====================================
+   // Profile (Auth)
+   // =====================================
    Route::get('profile', function () {
       return Inertia::render('Profile/Page');
    })->name('my-profile');
 
+
+   // =====================================
+   // Dashboard (Auth)
+   // =====================================
    Route::get('dashboard', function () {
       return Inertia::render('Dashboard/Page');
    })->name('dashboard');
 
+
+   // =====================================
+   // Dashboard Employee (Auth)
+   // =====================================
    Route::get('dashboard/employee', function () {
       return Inertia::render('Dashboard/employee/Page');
    })->name('dashboard/employe');
 
+
+   // =====================================
+   // Dashboard Kanban (Auth)
+   // =====================================
    Route::get('dashboard/kanban', function () {
       return Inertia::render('Dashboard/kanban/Page');
    })->name('dashboard/kanban');
 
+
+   // =====================================
+   // Dashboard Profile (Auth)
+   // =====================================
    Route::get('dashboard/profile', function () {
       return Inertia::render('Dashboard/profile/Page');
    })->middleware('checkRole:Admin', 'checkRole:Maba')->name('dashboard/profile');
 
-    Route::get('dashboard/user', function () {
-        return Inertia::render('Dashboard/user/Page');
-    })->name('dashboard/user');
 
+   // =====================================
+   // Dashboard User (Auth)
+   // =====================================
+   Route::get('dashboard/user', function () {
+      return Inertia::render('Dashboard/user/Page');
+   })->name('dashboard/user');
+
+
+   // =====================================
+   // Dashboard Atur Maba (Auth)
+   // =====================================
    Route::get('dashboard/atur-maba', function () {
       return Inertia::render('Dashboard/atur-maba/Page');
    })->name('dashboard/atur-maba');
 
+
+   // =====================================
+   // Dashboard Atur Dapmen (Auth)
+   // =====================================
    Route::get('dashboard/atur-dapmen', function () {
       return Inertia::render('Dashboard/atur-dapmen/Page');
    })->name('dashboard/atur-dapmen');
 
+
+   // =====================================
+   // Dashboard Atur Korlap (Auth)
+   // =====================================
    Route::get('dashboard/atur-korlap', function () {
       return Inertia::render('Dashboard/atur-korlap/Page');
    })->name('dashboard/atur-korlap');
 
+
+   // =====================================
+   // Dashboard Atur PJ Prodi (Auth)
+   // =====================================
    Route::get('dashboard/atur-pjprodi', function () {
       return Inertia::render('Dashboard/atur-pjprodi/Page');
    })->name('dashboard/atur-pjprodi');
 
+
+   // =====================================
+   // Dashboard Absensi Maba (Auth)
+   // =====================================
    Route::get('dashboard/absensi-maba', function () {
       return Inertia::render('Dashboard/absensi-maba/Page');
    })->name('dashboard/absensi-maba');
 
+
+   // =====================================
+   // Dashboard FAQ (Auth)
+   // =====================================
    Route::get('dashboard/faq', function () {
       return Inertia::render('Dashboard/faq/Page');
    })->name('dashboard/faq');
 
-    Route::get('dashboard/booklet', function () {
-        return Inertia::render('Dashboard/booklet/Page');
-    })->name('dashboard/booklet');
 
+   // =====================================
+   // Dashboard Booklet (Auth)
+   // =====================================
+   Route::get('dashboard/booklet', function () {
+      return Inertia::render('Dashboard/booklet/Page');
+   })->name('dashboard/booklet');
+
+
+   // =====================================
+   // Dashboard Mading (Auth)
+   // =====================================
    Route::get('dashboard/mading', function () {
       return Inertia::render('Dashboard/mading/Page');
    })->name('dashboard/mading');
 
+   // =====================================
+   // Dashboard CUI(Auth)
+   // =====================================
    Route::get('dashboard/cui/absensi', [PresensiCuiController::class, 'absensi'])
       ->name('dashboard.cui.absensi');
 
@@ -115,23 +238,26 @@ Route::middleware('auth')->group(function () {
       ->name('dashboard.cui');
 
    Route::post('dashboard/cui', [PresensiCuiController::class, 'getMabaByNim']);
-});
 
-Route::middleware('auth')->group(function () {
    // Route::get('dev', function () {
    //     return Inertia::render('Dev/Page', [
    //         'username' => auth()->user()->name,
    //         'password' => auth()->user()->password
    //     ]);
    // })->name('dev');
-   Route::get('dashboard/mading', function () {
-      return Inertia::render('Dashboard/mading/Page');
-   })->name('dashboard/mading');
+   
 
+   // =====================================
+   // Scoreboard (Auth)
+   // =====================================
    Route::get('scoreboard', function () {
       return Inertia::render('Scoreboard/Page');
    })->name('scoreboard');
 
+
+   // =====================================
+   // Relasi (Auth)
+   // =====================================
    Route::get('relasi', function () {
       return Inertia::render('Relasi/Page');
    })->name('relasi');
