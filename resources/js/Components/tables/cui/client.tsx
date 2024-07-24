3ad3ea86886af22347ae5c57e2fa9cfcc8d21a79
Cornelius Linux <radi.adi78@gmail.com>
2024-07-24 22:40:13 +0700

    feat: ketinggalan banh

--- resources/js/Components/tables/cui/client.tsx
+++ resources/js/Components/tables/cui/client.tsx
@@ -2,25 +2,10 @@
 
 import { columns } from "./columns";
 
-import { useState } from "react";
-
 import { DataTable } from "@/Components/ui/data-table";
-import { Heading } from "@/Components/ui/heading";
-// import { useRouter } from "next/navigation";
 import { Separator } from "@/Components/ui/separator";
 
 import { LogBookCui } from "@/lib/types/LogBookCui";
-
-interface ProductsClientProps {
-    data: LogBookCui[];
-    total: number;
-    perPage: number;
-    currentPage: number;
-    totalPages: number;
-    onPageChange: (page: number) => void;
-    onItemsPerPageChange: (itemsPerPage: number) => void;
-    onSearchChange: (searchTerm: string) => void;
-}
 
 export const LogBookClient = () => {
     return (
