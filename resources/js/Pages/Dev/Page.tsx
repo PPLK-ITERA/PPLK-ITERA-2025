7b6f5a196c976a490a02e14032d3b1fd92a70301
jo0707 <josua123690707@gmail.com>
2024-07-28 23:31:55 +0700

    feat: Update devcard.css and Page.tsx

--- resources/js/Pages/Dev/Page.tsx
+++ resources/js/Pages/Dev/Page.tsx
@@ -5,7 +5,7 @@ import DevCard from "@/Components/dev/DevCard";
 
 function Page() {
     return (
-        <div className="bg-mobile-hero-background md:min-h-screen md:bg-desktop-hero-background lg:bg-desktop-hero-background h-screen bg-bottom bg-cover">
+        <div className=" h-screen bg-bottom bg-cover">
             <DevCard></DevCard>
         </div>
     );
