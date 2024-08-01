ca584656640f9db738f9d6205ece756449e0b8a2
Gatzmoz <gilangminecraft21@gmail.com>
2024-08-02 03:04:25 +0700

    refactor: membersihkan code pilarcontroller

--- app/Http/Controllers/Pilar/PilarController.php
+++ app/Http/Controllers/Pilar/PilarController.php
@@ -16,16 +16,8 @@ class PilarController extends Controller
       $result = Result::where('user_id', auth()->user()->id)->firstOrFail();
       $pilar = null;
       $sifatScores = [$result->sifat_1_score, $result->sifat_2_score, $result->sifat_3_score];
-      $users = User::where('role_id', 1)->whereNotNull('pilar')->count();
-      $pilar1 = User::where('role_id', 1)->where('pilar', 1)->whereNotNull('pilar')->count();
-      $pilar2 = User::where('role_id', 1)->where('pilar', 2)->whereNotNull('pilar')->count();
-      $pilar3 = User::where('role_id', 1)->where('pilar', 3)->whereNotNull('pilar')->count();
-      $pilar4 = User::where('role_id', 1)->where('pilar', 4)->whereNotNull('pilar')->count();
 
-      $persen1 = ($pilar1 / $users) * 100;
-      $persen2 = ($pilar2 / $users) * 100;
-      $persen3 = ($pilar3 / $users) * 100;
-      $persen4 = ($pilar4 / $users) * 100;
+
 
 
       $user = User::findOrFail(auth()->user()->id);
@@ -41,15 +33,10 @@ class PilarController extends Controller
          $user->pilar = $pilarIndex + 1;
          $user->save();
       }
-      if ($pilar->id == 1) {
-         $persen = $persen1;
-      } elseif ($pilar->id == 2) {
-         $persen = $persen2;
-      } elseif ($pilar->id == 3) {
-         $persen = $persen3;
-      } else {
-         $persen = $persen4;
-      }
+      $users = User::where('role_id', 1)->whereNotNull('pilar')->count();
+      $userpilar = User::where('role_id', 1)->where('pilar', $pilarIndex + 1)->whereNotNull('pilar')->count();
+
+      $persen = ($userpilar / $users) * 100;
 
       return Inertia::render('Asesmen/Result/Page', [
          'response' => [
