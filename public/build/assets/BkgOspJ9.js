cd4fe453d524ccb512f31b961a4f504a6fee6e2b
Gatzmoz <gilangminecraft21@gmail.com>
2024-08-02 17:27:22 +0700

    feat: build new assets for staging

---
+++ public/build/assets/BkgOspJ9.js
@@ -0,0 +1,1 @@
+import{j as r}from"./B1esdhcH.js";function o(){return r.jsx("div",{children:"DashboardPJProdi"})}export{o as default};
--- public/build/assets/ClczN8oy.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./CE_OarKu.js";function e(){return r.jsx("div",{children:"Scoreboard Page"})}export{e as default};
