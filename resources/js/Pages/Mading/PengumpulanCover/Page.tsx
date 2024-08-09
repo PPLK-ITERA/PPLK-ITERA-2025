e0c1238887ff5f7dfa503f4e5af4f85897d4a1aa
Cornelius Linux <radi.adi78@gmail.com>
2024-08-10 04:57:39 +0700

    feat: mading done

--- resources/js/Pages/Mading/PengumpulanCover/Page.tsx
+++ resources/js/Pages/Mading/PengumpulanCover/Page.tsx
@@ -14,6 +14,15 @@ import DefaultLayout from "@/Layouts/DefaultLayout";
 
 import MaxWidthWrapper from "@/Components/MaxWidthWrapper";
 import { Button, buttonVariants } from "@/Components/ui/button";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/Components/ui/dialog";
 import { Input } from "@/Components/ui/input";
 import { Label } from "@/Components/ui/label";
 import { Toaster } from "@/Components/ui/toaster";
@@ -21,13 +30,15 @@ import { useToast } from "@/Components/ui/use-toast";
 
 import { CardType } from "@/lib/types/Mading";
 
+import contohCover from "!assets/contoh_cover.png";
+
 const ketentuanUploadCover = [
     "Cover harus berbentuk kolase foto, seperti kolase kumpulan tugas, kolase foto saat kalian bekerja dalam kelompok, dan sebagainya.",
     "Desain cover bebas, sesuai dengan kreativitas kelompok.",
     "Kalian dapat membuat cover menggunakan Canva, Figma, atau aplikasi editing lain.",
     "Pastikan ukuran cover adalah 700x1000 piksel.",
-    "Pastikan kalian membuat cover yang sesuai, tidak boleh mengandung SARA, serta harus sopan dan mencerminkan nilai-nilai yang positif."
-    "Contoh cover seperti dibawah ini. Semangat Naramuda!"
+    "Pastikan kalian membuat cover yang sesuai, tidak boleh mengandung SARA, serta harus sopan dan mencerminkan nilai-nilai yang positif.",
+    "Contoh cover seperti dibawah ini. Semangat Naramuda!",
 ];
 
 export default function Page({ hari }) {
@@ -148,10 +159,30 @@ export default function Page({ hari }) {
                                         ),
                                     )}
                                 </div>
-
-                                <Button className="bg-jaffa-700 hover:bg-jaffa-700/90 mt-5 transition duration-200">
-                                    Contoh cover yang sesuai
-                                </Button>
+                                <Dialog>
+                                    <DialogTrigger asChild>
+                                        <Button className="bg-jaffa-700 hover:bg-jaffa-700/90 mt-5 transition duration-200">
+                                            Contoh Cover
+                                        </Button>
+                                    </DialogTrigger>
+                                    <DialogContent className="sm:max-w-[425px] h-fit">
+                                        <DialogHeader>
+                                            <DialogTitle>
+                                                Contoh Cover Mading
+                                            </DialogTitle>
+                                            <DialogDescription>
+                                                Berikut adalah contoh cover
+                                                mading yang baik dan benar.
+                                            </DialogDescription>
+                                        </DialogHeader>
+                                        <div className="flex justify-center">
+                                            <img
+                                                src={contohCover}
+                                                className="w-[85%]"
+                                            />
+                                        </div>
+                                    </DialogContent>
+                                </Dialog>
                             </div>
 
                             <div className="max-w-fit flex flex-col mx-auto">
