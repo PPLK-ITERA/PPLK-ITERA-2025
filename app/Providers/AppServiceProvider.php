86b8163168f705367f03ac636612b957a82f53f7
reannn22 <reyhan.123140022@student.itera.ac.id>
2025-07-23 23:13:08 +0700

    mine

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,26 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Http\JsonResponse;
 
 class AppServiceProvider extends ServiceProvider
 {
-   /**
-    * Register any application services.
-    */
-   public function register(): void
-   {
-      //
-   }
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
 
-   /**
-    * Bootstrap any application services.
-    */
-   public function boot(): void
-   {
-      //
-   }
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        JsonResponse::macro('formatted', function () {
+            $this->setEncodingOptions(JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+            return $this;
+        });
+    }
 }
