7ef3c06eff91bdd4d22d45a491b697306b8158d8
reannn22 <reyhan.123140022@student.itera.ac.id>
2025-08-05 00:59:38 +0700

    fixed

--- resources/js/Components/opening/Openingpage.tsx
+++ resources/js/Components/opening/Openingpage.tsx
@@ -266,7 +266,7 @@ const OpeningPage = ({ onComplete }) => {
                                 />
                                 <div className="absolute inset-0 rounded-2xl"></div>
                                 <div className="absolute inset-0 flex items-center justify-center">
-                                    <h3 className="font-bold text-white tracking-wide drop-shadow-xl text-lg font-greek md:text-3xl text-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full">
+                                    <h3 className="font-bold text-white tracking-wide drop-shadow-xl text-2xl font-greek md:text-4xl lg:text-5xl text-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full">
                                         {images[currentIndex].year}
                                     </h3>
                                 </div>
