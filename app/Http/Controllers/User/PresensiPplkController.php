5c4ee1939160d99764a2bb18eee6486971cdc977
Stevanus Cahya Anggara <stevanuschyaanggara@gmail.com>
2025-08-13 15:44:02 +0700

    update : tanggal presensi

--- app/Http/Controllers/User/PresensiPplkController.php
+++ app/Http/Controllers/User/PresensiPplkController.php
@@ -21,7 +21,7 @@ class PresensiPplkController extends Controller
    
    // Jam operasional presensi
    const JAM_MULAI_PRESENSI = 6;  // 6 AM
-   const JAM_AKHIR_PRESENSI = 13; // 1 PM
+   const JAM_AKHIR_PRESENSI = 17; // 1 PM
    
    // Tanggal khusus untuk role_id = 6 (Korlap)
    // Tanggal khusus untuk role_id = 5 (PJ Prodi)
